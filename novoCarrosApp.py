9ace0bbd314f1c5c8e9db5f2af8ab28e6951eeeb
Luan Ventura Ferreira de Moura <luan.moura43470@edu.pe.senac.br>
2025-11-14 17:08:27 -0300

    novoCarrosApp with POO

--- novoCarrosApp.py
+++ novoCarrosApp.py
@@ -1,56 +1,79 @@
 import json
 
-carros = []
+class Carros:
+    def __init__(self, marca, modelo, ano, preco):
+        self.marca = marca
+        self.modelo = modelo
+        self.ano = ano
+        self.preco = preco
+    
+    def lerCarros(self):
+        try:
+            with open("carros.json", "r", encoding="utf-8") as arquivo:
+                dadosDoJson = json.load(arquivo) 
+                return dadosDoJson
+        except FileNotFoundError:
+            print("O arquivo json nao foi encontrado.")
+            return {}
+        except json.JSONDecodeError:
+            print("Erro: O arquivo contém um erro de formato.")
+            return {}
+    
+    def printCarros(self):
+        carros = self.lerCarros()
+        #a lista 'carros' fica atualizada com os dados do carros.json
+        print("Dados que foram carregados do JSON 'carros': ")
+        print(carros)
 
-# Criação de um CRUD.    Abaixo temos o READ:
-def lerCarros():
-    try:
-        with open("carros.json", "r", encoding="utf-8") as arquivo:
-            dadosDoJson = json.load(arquivo) 
-            return dadosDoJson
-    except FileNotFoundError:
-        print("O arquivo json nao foi encontrado.")
-        return {}
-    except json.JSONDecodeError:
-        print("Erro: O arquivo contém um erro de formato.")
-        return {}
+    def inserirCarros(self):
+        carrosExistentes = self.lerCarros()
+        dicionarioCarros = {
+            "marca": self.marca,
+            "modelo": self.modelo,
+            "ano": self.ano,
+            "preco": self.preco
+        }
 
-def printCarros():    
-    carros = lerCarros()
-    #a lista 'carros' fica atualizada com os dados do carros.json
-    print("Dados que foram carregados do JSON 'carros': ")
-    print(carros)
+        #Criacao do ID (ele pega a quantidade de IDS dentro de 'carrosExistentes' e define eles como chave para add os outros dicionarios)
+        novo_id = (str(len(carrosExistentes)+ 1))
+        carrosExistentes[novo_id] = dicionarioCarros
 
-def inserirCarros():
-    carrosExistentes = lerCarros()
+        with open("carros.json", "w", encoding="utf-8") as arquivo:
+            json.dump(carrosExistentes, arquivo, ensure_ascii=False, indent=4 )
+            print("Dados salvos com sucesso!")
+
+while True:
+    
     marca = input("Digite a marca do carro: ")
     modelo = input("Digite o modelo do carro: ")
-    try:
-        ano = int(input("Digite o ano que o carro foi fabricado: "))
-    except ValueError:
-        print("O valor digitado para o ano não é um inteiro.")
-        ano = int(input("Digite o ano que o carro foi fabricado: "))
-        return ano
-    
-    try:
-        preco = float(input("Digite o preco do carro: "))
-    except ValueError:
-        print("O valor digitado para o preço nao segue o padrão float, digite novamente")
-        preco = float(input("Digite o preco do carro: "))
-        return preco
-    dicionarioCarros = {
-        "marca": marca,
-        "modelo": modelo,
-        "ano": ano,
-        "preco": preco
-    }
-    #Criacao do ID (ele pega a quantidade de IDS dentro de 'carrosExistentes' e define eles como chave para add os outros dicionarios)
-    novo_id = (str(len(carrosExistentes)+ 1))
-    carrosExistentes[novo_id] = dicionarioCarros
+    ano = int(input("Digite o ano que o carro foi fabricado: "))  
+    preco = float(input("Digite o preco do carro: "))
+    carro1 = Carros(marca, modelo, ano, preco)
+    carro1.inserirCarros()
 
-    with open("carros.json", "w", encoding="utf-8") as arquivo:
-        json.dump(carrosExistentes, arquivo, ensure_ascii=False, indent=4 )
-        print("Dados salvos com sucesso!")
+# Criação de um CRUD.    Abaixo temos o READ:
 
-inserirCarros()
-printCarros()
+# def lerCarros():
+#     try:
+#         with open("carros.json", "r", encoding="utf-8") as arquivo:
+#             dadosDoJson = json.load(arquivo) 
+#             return dadosDoJson
+#     except FileNotFoundError:
+#         print("O arquivo json nao foi encontrado.")
+#         return {}
+#     except json.JSONDecodeError:
+#         print("Erro: O arquivo contém um erro de formato.")
+#         return {}
+
+# def printCarros():    
+#     carros = lerCarros()
+#     #a lista 'carros' fica atualizada com os dados do carros.json
+#     print("Dados que foram carregados do JSON 'carros': ")
+#     print(carros)
+
+
+
+# inserirCarros()
+# printCarros()
+
+
