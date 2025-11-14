43122fc7bb0fa85145934668980a08976818c562
luanvfm <luanventurafm29@gmail.com>
2025-11-13 21:00:37 -0300

    feat: CRUD novoCarrosApp finalizado

--- novoCarrosApp.py
+++ novoCarrosApp.py
@@ -10,33 +10,47 @@ def lerCarros():
             return dadosDoJson
     except FileNotFoundError:
         print("O arquivo json nao foi encontrado.")
-        return []
+        return {}
     except json.JSONDecodeError:
         print("Erro: O arquivo contém um erro de formato.")
-        return []
-    
-carros = lerCarros()
-#a lista 'carros' fica atualizada com os dados do carros.json
-print("Dados que foram carregados do JSON 'carros': ")
-print(carros)
+        return {}
+
+def printCarros():    
+    carros = lerCarros()
+    #a lista 'carros' fica atualizada com os dados do carros.json
+    print("Dados que foram carregados do JSON 'carros': ")
+    print(carros)
 
 def inserirCarros():
+    carrosExistentes = lerCarros()
+    marca = input("Digite a marca do carro: ")
+    modelo = input("Digite o modelo do carro: ")
+    try:
+        ano = int(input("Digite o ano que o carro foi fabricado: "))
+    except ValueError:
+        print("O valor digitado para o ano não é um inteiro.")
+        ano = int(input("Digite o ano que o carro foi fabricado: "))
+        return ano
+    
+    try:
+        preco = float(input("Digite o preco do carro: "))
+    except ValueError:
+        print("O valor digitado para o preço nao segue o padrão float, digite novamente")
+        preco = float(input("Digite o preco do carro: "))
+        return preco
+    dicionarioCarros = {
+        "marca": marca,
+        "modelo": modelo,
+        "ano": ano,
+        "preco": preco
+    }
+    #Criacao do ID (ele pega a quantidade de IDS dentro de 'carrosExistentes' e define eles como chave para add os outros dicionarios)
+    novo_id = (str(len(carrosExistentes)+ 1))
+    carrosExistentes[novo_id] = dicionarioCarros
+
     with open("carros.json", "w", encoding="utf-8") as arquivo:
-        marca = input("Digite a marca do carro: ")
-        modelo = input("Digite o modelo do carro: ")
-        try:
-            ano = int(input("Digite o ano que o carro foi fabricado: "))
-        except ValueError:
-            print("O valor digitado para o ano não é um inteiro.")
-            ano = int(input("Digite o ano que o carro foi fabricado: "))
-            return ano
-        
-        try:
-            preco = float(input("Digite o preco do carro: "))
-        except ValueError:
-            print("O valor digitado para o preço nao segue o padrão float, digite novamente")
-            preco = float(input("Digite o preco do carro: "))
-            return preco
+        json.dump(carrosExistentes, arquivo, ensure_ascii=False, indent=4 )
+        print("Dados salvos com sucesso!")
 
-
-        insereCarro = json.dump()
+inserirCarros()
+printCarros()
