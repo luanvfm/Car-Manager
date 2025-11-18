770cf3613cece1fb775a65618d7ba37d8ff58944
luanvfm <luanventurafm2@gmail.com>
2025-11-18 15:34:31 -0300

    creation of the Manager Class and redistribution of methods

--- novoCarrosApp.py
+++ novoCarrosApp.py
@@ -6,7 +6,32 @@ class Carros:
         self.modelo = modelo
         self.ano = ano
         self.preco = preco
-    
+
+
+class Gerenciador:
+    def inserirCarros(self, carro_novo):
+        carrosExistentes = self.lerCarros()
+        dicionarioCarros = {
+            "marca": carro_novo.marca,
+            "modelo": carro_novo.modelo,
+            "ano": carro_novo.ano,
+            "preco": carro_novo.preco
+        }
+
+        #Criacao do ID (ele pega a quantidade de IDS dentro de 'carrosExistentes' e define eles como chave para add os outros dicionarios)
+        novo_id = (str(len(carrosExistentes)+ 1))
+        carrosExistentes[novo_id] = dicionarioCarros
+
+        with open("carros.json", "w", encoding="utf-8") as arquivo:
+            json.dump(carrosExistentes, arquivo, ensure_ascii=False, indent=4 )
+            print("Dados salvos com sucesso!")
+
+    def printCarros(self):
+        carros = self.lerCarros()
+        #a lista 'carros' fica atualizada com os dados do carros.json
+        print("Dados que foram carregados do JSON 'carros': ")
+        print(carros)
+
     def lerCarros(self):
         try:
             with open("carros.json", "r", encoding="utf-8") as arquivo:
@@ -18,38 +43,23 @@ class Carros:
         except json.JSONDecodeError:
             print("Erro: O arquivo contém um erro de formato.")
             return {}
-    
-    def printCarros(self):
-        carros = self.lerCarros()
-        #a lista 'carros' fica atualizada com os dados do carros.json
-        print("Dados que foram carregados do JSON 'carros': ")
-        print(carros)
 
-    def inserirCarros(self):
-        carrosExistentes = self.lerCarros()
-        dicionarioCarros = {
-            "marca": self.marca,
-            "modelo": self.modelo,
-            "ano": self.ano,
-            "preco": self.preco
-        }
-
-        #Criacao do ID (ele pega a quantidade de IDS dentro de 'carrosExistentes' e define eles como chave para add os outros dicionarios)
-        novo_id = (str(len(carrosExistentes)+ 1))
-        carrosExistentes[novo_id] = dicionarioCarros
-
-        with open("carros.json", "w", encoding="utf-8") as arquivo:
-            json.dump(carrosExistentes, arquivo, ensure_ascii=False, indent=4 )
-            print("Dados salvos com sucesso!")
-
+gerente = Gerenciador()
 while True:
-    
-    marca = input("Digite a marca do carro: ")
-    modelo = input("Digite o modelo do carro: ")
-    ano = int(input("Digite o ano que o carro foi fabricado: "))  
-    preco = float(input("Digite o preco do carro: "))
-    carro1 = Carros(marca, modelo, ano, preco)
-    carro1.inserirCarros()
+    continuar = input("\n Gostaria de digitar um novo Carro? Caso não queira deixe em branco: ")
+    if continuar != "":
+        marca = input("Digite a marca do carro: ")
+        modelo = input("Digite o modelo do carro: ")
+        ano = int(input("Digite o ano: "))  
+        preco = float(input("Digite o preco: "))
+
+        carro1 = Carros(marca, modelo, ano, preco)
+
+        gerente.inserirCarros(carro1)
+    else:
+        print("")
+        print("Fim do programa")
+        break
 
 # Criação de um CRUD.    Abaixo temos o READ:
 
